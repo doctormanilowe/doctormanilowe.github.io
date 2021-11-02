4ca77627e8dcc07c6104a57749af903590ed7ee6
Andrew Campbell <AndyC@andrews-mbp.home>
2021-11-01 23:18:43 -0700

    add logic for naise quiz

--- index.js
+++ index.js
@@ -34,47 +34,84 @@ const questions = [
     },
     {
         question: `Doctor Manilowe suggests adding _____ to your Doctor Manilowe's Vegan Mayo for a little extra zing?`,
-        options: [`Fried Chicken (Hint: This answer is morally wrong!)`, `Hard Cider`, `Lemon`, `Baby Aspirin`],
+        options: [
+            {
+                desc: `Fried Chicken (Hint: This answer is morally wrong!)`,
+                msg: rand(msgWrong)
+            },
+            {
+                desc: `Hard Cider`,
+                msg: rand(msgWrong)
+            },
+            {
+                desc: `Lemon`,
+                msg: `Naisemaste! That is the correct answer!`
+            },
+            {
+                desc: `Baby Aspirin`,
+                msg: rand(msgWrong)
+            },
+        ],
         correct: "Lemon"
     },
-    {
-        question: `Complete this phrase: "Quick, thick and _______"`,
-        options: [`Hot`, `Fat-Free`, `Naisemaste`, `Vegan`],
-        correct: "Vegan"
-    },
-    {
-        question: `Complete this phrase: "Quick, thick and _______"`,
-        options: [`Hot`, `Fat-Free`, `Naisemaste`, `Vegan`],
-        correct: "Vegan"
-    },
-    {
-        question: `Complete this phrase: "Quick, thick and _______"`,
-        options: [`Hot`, `Fat-Free`, `Naisemaste`, `Vegan`],
-        correct: "Vegan"
-    },
-    {
-        question: `Complete this phrase: "Quick, thick and _______"`,
-        options: [`Hot`, `Fat-Free`, `Naisemaste`, `Vegan`],
-        correct: "Vegan"
-    },
-    {
-        question: `Complete this phrase: "Quick, thick and _______"`,
-        options: [`Hot`, `Fat-Free`, `Naisemaste`, `Vegan`],
-        correct: "Vegan"
-    },
-    {
-        question: `Complete this phrase: "Quick, thick and _______"`,
-        options: [`Hot`, `Fat-Free`, `Naisemaste`, `Vegan`],
-        correct: "Vegan"
-    },
-    {
-        question: `Complete this phrase: "Quick, thick and _______"`,
-        options: [`Hot`, `Fat-Free`, `Naisemaste`, `Vegan`],
-        correct: "Vegan"
-    },
-    {
-        question: `Complete this phrase: "Quick, thick and _______"`,
-        options: [`Hot`, `Fat-Free`, `Naisemaste`, `Vegan`],
-        correct: "Vegan"
-    },
+    
 ]
+
+// Quiz Logic
+let options = []
+let correct = '';
+// check answer
+const checkAnswer = () => {
+    const answer = document.querySelector('input[name="question"]:checked').value;
+    const { msg } = options.find(option => option.desc === answer);
+    alert(msg);
+    if(answer === correct) {
+        [ options, correct ] = resetQuiz();
+    }
+}
+// create DOM elements for quiz question
+const resetQuiz = () => {
+    // clear quiz
+    document.getElementById('quiz-container').innerText = "";
+    // get new question
+    const { question, options, correct} = rand(questions);
+    const quizContainer = document.getElementById('quiz-container');
+    const newQuestion = document.createElement('p')
+    const multiChoice = document.createElement('ol');
+    multiChoice.classList.add('quiz-multi-choice');
+    for (let option of options) {
+        const i = options.indexOf(option);
+        const li = document.createElement('li');
+        const input = document.createElement('input');
+        const label = document.createElement('label');
+        label.htmlFor = `multiChoice${i}`
+        label.innerText = option.desc
+        input.id = `multiChoice${i}`
+        input.type = 'radio';
+        input.name = 'question';
+        input.value = option.desc;
+        li.append(input);
+        li.append(label);
+        multiChoice.append(li);
+    }
+    quizContainer.innerText = "";
+    newQuestion.innerText = question;
+    quizContainer.append(newQuestion);
+    quizContainer.append(multiChoice);
+    // add event listener to radios to enable the check answer button when an option is chosen
+    if (document.querySelector('input[name="question"]')) {
+        document.querySelectorAll('input[name="question"]').forEach((elem) => {
+            elem.addEventListener("change", function(e) {
+            if(e.target.value) {
+                document.getElementById('quiz-check-answer').removeAttribute('disabled');
+            }
+            });
+        });
+    }
+    return [options, correct];
+}
+// add event listener to check answer button
+document.getElementById('quiz-check-answer').addEventListener('click', checkAnswer);
+
+[ options, correct ] = resetQuiz();
+
